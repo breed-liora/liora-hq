92c9a5e27ff702e9a2cfc62485ebda4669db10b0
Your Name <you@example.com>
2024-09-16 13:24:47 -0400

    feat: Add Scheduler component and SchedulingTable

---
+++ src/apps/Scheduler/Scheduler.js
@@ -0,0 +1,14 @@
+// src/components/Scheduler.js
+import React from 'react';
+import SchedulingTable from './SchedulingTable';
+
+function Scheduler() {
+  return (
+    <div>
+      <h2>Scheduler</h2>
+      <SchedulingTable />
+    </div>
+  );
+}
+
+export default Scheduler;
